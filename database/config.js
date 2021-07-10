32fd5c39b694043c5e112b8ac16a3c6e4291baf4
root <agaillard88@gmail.com>
2021-07-10 16:44:27 -0400

    Added config to gitignore

--- database/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  user: 'postgres',
-  password: 'masterkey',
-  database: 'hammerhead'
-};
--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg')
-const pgConfig = require('./config.js');
+const pgConfig = require('../config.js');
 const client = new Client(pgConfig)
 
 client.connect()
