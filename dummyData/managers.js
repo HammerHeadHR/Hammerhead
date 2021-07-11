86f004ed89df99a7a5eba79944d595866eec8c37
Alex A <alexaarms96@gmail.com>
2021-07-11 11:31:40 -0400

    added dummyData directory and files
    made mock api request results from the example data we were given

---
+++ dummyData/managers.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "managers" : [
+    {
+      "name" : "Ruby",
+      "team" : "Agriculture"
+    },
+    {
+      "name" : "Samantha",
+      "team" : "FinTech"
+    },
+    {
+      "name" : "Hilary",
+      "team" : "Retails"
+    },
+    {
+      "name" : "Bob",
+      "team" : "Healthcare"
+    }
+  ]
+};
