3cc43a877bbb63a3027e0f90eef1725ab97f15df
Alex Armstrong <alexaarms96@gmail.com>
2021-07-12 15:56:05 -0400

    wrote get requests for fetching teams and employees

--- client/src/components/EmployeeManagement/index.jsx
+++ client/src/components/EmployeeManagement/index.jsx
@@ -1,14 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import EmployeeList from './EmployeeList.jsx';
 import TeamList from './TeamList.jsx';
+import axios from 'axios';
 
 const EmployeeManagement = ({ employees, managers }) => {
 
   const [employeeList, setEmployeeList] = useState(employees);
+  const [teamList, setTeamList] = useState(managers);
+
+
+  // const getEmployees = () => {
+  //   axios.get('/employees')
+  //     .then(({ data }) => {
+  //       setEmployeeList(data);
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // }
+
+  // const getTeams = () => {
+  //   axios.get('/teams')
+  //   .then(({ data }) => {
+  //     setTeamList(data);
+  //   })
+  //   .catch(error => {
+  //     console.error(error);
+  //   });
+  // }
+
+  // useEffect(() => {
+  //   getEmployees();
+  //   getTeams();
+  // }, []);
 
   return (
     <div>
-      <TeamList teams={managers}/>
+      <TeamList teams={teamList}/>
       <EmployeeList employees={employeeList} setEmployees={setEmployeeList}/>
     </div>
   );
