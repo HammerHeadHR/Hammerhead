1a63743ccce36248400e19bd7c246d3aa09fbba0
Katherine Darby <katherinemdarby@gmail.com>
2021-07-12 19:09:32 -0400

    Wonky Bug in Graph Component

--- client/src/components/SharedComponents/Graph.jsx
+++ client/src/components/SharedComponents/Graph.jsx
@@ -2,51 +2,80 @@ import React, {useState, useEffect} from 'React';
 import { BarChart, Bar, Cell, AreaChart, Area, LineChart, Line, Legend, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 
-const DynamicGraphWrapper = (data) => {
+
+const GraphKey = ({propertyName}) => {
+  const [current, setCurrent] = useState('');
+  const [color, setColor] = useState('');
+
+  const handleChange = (e) => {
+    console.log(e.target.name);
+  };
+
   return (
-    
+    <>
+      <p>{propertyName}</p>
+      <label>X Axis</label>
+      <input type="radio" name="xAxis" onChange={handleChange}></input>
+      <label>Rendered Data</label>
+      <input type="radio" name="data" onChange={handleChange}></input>
+      {current === 'data' ? <input type="color"></input> : null}
+    </>
   )
-}
+};
 
 
 
 const Graph = ({data, xAxis, keys}) => {
-  // console.log(data.prices);
-  // let keys = [];
 
-  // for (let key in data) {
-  //   if (key !== 'title' && key !== 'managers') {
-  //     for (let property in data[key][0]) {
-  //       keys.push(property);
-  //     }
-  //   }
-  // }
-
-  // let keysMinusX = keys.slice(1);
-  // console.log(keys);
-
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data.prices}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey={xAxis} />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          {keys.map(key => { return <Line type="monotone" dataKey={keys[2]} stroke="#8884d8" activeDot={{ r: 8 }} /> })}
-        </LineChart>
-      </ResponsiveContainer>
-    );
-}
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey={xAxis} />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        {keys.map(key => { return <Line type="monotone" dataKey={key} stroke="#8884d8" activeDot={{ r: 8 }} /> })}
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
 
-export default Graph;
+const DynamicGraphWrapper = ({data}) => {
+
+
+  const [xAxis, setXAxis] = useState('year');
+  const [keys, setKeys] = useState(['price']);
+  const [dataKeys, setDataKeys] = useState([]);
+
+
+  for (let key in data[0]) {
+    setDataKeys(prev => {
+      return [...prev, key];
+    })
+  }
+
+  return (
+    <div>
+      <div>
+        {dataKeys.map((property, i) => { return <GraphKey propertyName={property} key={i}/>})}
+      </div>
+      <div style={{height: '40vh', width: '30vw'}}>
+        <Graph xAxis={dataKeys[0]} keys={dataKeys.slice(1)} data={data}/>
+      </div>
+    </div>
+  )
+};
+
+
+export default DynamicGraphWrapper;
