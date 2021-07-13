449baf670394dee0e9c30f126a82d1e5eef00a22
Katherine Darby <katherinemdarby@gmail.com>
2021-07-13 17:19:00 -0400

    broken login page

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App.jsx';
+import App from './App.jsx';
 import Example from './Example.jsx';
 
-ReactDOM.render(<Example />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
