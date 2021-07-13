a91819e4cd12129a776b7eedd1ad8275065a7a7f
Alex Armstrong <alexaarms96@gmail.com>
2021-07-13 19:18:46 -0400

    added base login function

--- client/src/components/Login/LoginForm.jsx
+++ client/src/components/Login/LoginForm.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 const LoginForm = () => {
+
+  const login = (event) => {
+    event.preventDefault();
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+    axios.post('/login', { username: username, password: password })
+      .then(response => console.log(response))
+      .catch(error => console.error(error));
+  };
+
   return (
     <div>
-      <form action="/login" method="POST">
+      <form >
         <label htmlFor="username">Username:</label>
-        <input type="text" name="username"/>
+        <input type="text" name="username" id="username"/>
         <label htmlFor="password">Password:</label>
-        <input type="password" name="password"/>
+        <input type="password" name="password" id="password"/>
         <input type="submit" value="Log In"/>
       </form>
     </div>
   );
+
 };
 
 export default LoginForm;
