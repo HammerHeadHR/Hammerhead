830ec8db455adb3ab5fb8c55a3ae2e604087629d
Alex Armstrong <alexaarms96@gmail.com>
2021-07-14 01:32:44 -0400

    removed console.log

--- client/src/components/Login/LoginForm.jsx
+++ client/src/components/Login/LoginForm.jsx
@@ -10,7 +10,6 @@ const LoginForm = ({ setUser, setAuthed }) => {
     if (!username || !password) return alert('Please enter a username and password.');
     axios.post('/login', { username: username, password: password })
       .then(response => {
-        console.log(response)
         setUser(response.data);
         setAuthed(true);
       })
