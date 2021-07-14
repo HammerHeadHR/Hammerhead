2ea0720efb3987ce9430b902f433e9a9a8306837
Alex Armstrong <alexaarms96@gmail.com>
2021-07-14 18:12:33 -0400

    removed context test

--- client/src/components/EmployeeManagement/Employee.jsx
+++ client/src/components/EmployeeManagement/Employee.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import axios from 'axios';
-import { UserContext } from '../../App.jsx';
 
 const Employee = ({ employee, setEmployees, editEmployee, getEmployees }) => {
 
@@ -14,13 +13,8 @@ const Employee = ({ employee, setEmployees, editEmployee, getEmployees }) => {
       .catch(error => console.error(error));
   };
 
-  const contextTest = () => {
-    console.log(user);
-  }
-
   return (
     <div>
-      <button onClick={contextTest}>Context Log</button>
       <p>{employee.username} - {employee.team}</p>
       {employee.active && <button onClick={() => editEmployee(employee)}>Update Employee</button>}
       <button onClick={toggleEmployeeAccess}>{employee.active ? 'Revoke Access' : 'Grant Access'}</button>
