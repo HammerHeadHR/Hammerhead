7ec5ca7773e7b32c08c72abc39b4477b57939840
Alex Armstrong <alexaarms96@gmail.com>
2021-07-16 20:45:44 -0400

    changed logout route from get to post

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.clearCookie('_hh4DcT')
+router.post('/', (req, res) => {
+  res.clearCookie('_hh4DcT');
   res.redirect('/');
 });
 
